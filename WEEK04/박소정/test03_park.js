bae52a6bd008dc9e6082e32e13c1ff4830138dcc
hanzuneee <hanzuneee@naver.com>
2024-03-20 22:24:36 +0900

    second commit

--- WEEK04/박소정/test03_park.js
+++ WEEK04/박소정/test03_park.js
@@ -1,3 +1,35 @@
-document.querySelector('p_name').addEventListener('click', function p_btn_name(){
+document.querySelector('.p_name').addEventListener('click', function p_btn_name(){
+    personalbar.name = '박소정';
+    console.log(person);
+    });
 
-});
+document.querySelector('.p_age').addEventListener('click', function p_btn_name(){
+    personalbar.name = '21살';
+    console.log(person);
+    });
+
+document.querySelector('.p_hobby').addEventListener('click', function p_btn_name(){
+    personalbar.name = '노래듣기';
+    console.log(person);
+    });
+
+document.querySelector('.p_color').addEventListener('click', function p_btn_name(){
+    personalbar.name = '남색';
+    console.log(person);
+    });
+
+document.querySelector('.m_name').addEventListener('click', function m_btn_name(){
+    console(person);
+    });
+
+document.querySelector('.m_age').addEventListener('click', function m_btn_name(){
+    console(person);
+    });
+    
+document.querySelector('.m_hobby').addEventListener('click', function m_btn_name(){
+    console(person);
+    });
+
+document.querySelector('.m_color').addEventListener('click', function m_btn_name(){
+    console(person);
+    });
